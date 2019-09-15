b4aff471358d0cd8ab9694850a07a466690897c8
John Isaac Cunningham <jicunnin@calpoly.edu>
2019-09-15 15:18:15 -0700

    add new files, implement vec3, rename to hpp

--- ray_tracing_in_one_weekend.cpp
+++ ray_tracing_in_one_weekend.cpp
@@ -2,11 +2,36 @@
 //
 
 #include "ray_tracing_in_one_weekend.h"
+#include "vec3.h"
+
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+
+	ofstream imageFile;
+	imageFile.open("test.ppm", ios::out | ios::trunc);
+
+	int nx = 200;
+	int ny = 100;
+	imageFile << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--)
+	{
+		for (int i = 0; i < nx; i++)
+		{
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
+
+			int ir = int(255.99 * col[0]);
+			int ig = int(255.99 * col[1]);
+			int ib = int(255.99 * col[2]);
+			imageFile << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+
+	imageFile.close();
+
 	return 0;
 }
