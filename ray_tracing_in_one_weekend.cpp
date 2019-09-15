fa0883eaea58c785c9743ff7a1e81f1efb232f24
John Isaac Cunningham <jicunnin@calpoly.edu>
2019-09-15 14:18:00 -0700

    first commit

---
+++ ray_tracing_in_one_weekend.cpp
@@ -0,0 +1,12 @@
+﻿// ray_tracing_in_one_weekend.cpp : Defines the entry point for the application.
+//
+
+#include "ray_tracing_in_one_weekend.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
