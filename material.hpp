a087eff0041d8ba3b3ecd3769d5556fa58bb6ad7
John Isaac Cunningham <jicunnin@calpoly.edu>
2019-09-16 00:15:10 -0700

    add configurable camera

--- material.hpp
+++ material.hpp
@@ -19,6 +19,13 @@ bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted)
 	return false;
 }
 
+float schlick(float cosine, float ref_idx)
+{
+	float r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0) * pow((1 - cosine), 5);
+}
+
 vec3 random_in_unit_sphere() {
 	vec3 p;
 	do {
@@ -60,37 +67,47 @@ public:
 	float fuzz;
 };
 
-class dielectric : public material
-{
+class dielectric : public material {
 public:
-	dielectric(float ri) : ref_idx(ri) {};
-	virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const
-	{
+	dielectric(float ri) : ref_idx(ri) {}
+	virtual bool scatter(const ray& r_in, const hit_record& rec,
+		vec3& attenuation, ray& scattered) const {
 		vec3 outward_normal;
 		vec3 reflected = reflect(r_in.direction(), rec.normal);
-		attenuation = vec3(1.0f);
-		vec3 refracted;
 		float ni_over_nt;
+		attenuation = vec3(1.0, 1.0, 1.0);
+		vec3 refracted;
 
-		if (dot(r_in.direction(), rec.normal) > 0)
-		{
+		float reflect_prob;
+		float cosine;
+
+		if (dot(r_in.direction(), rec.normal) > 0) {
 			outward_normal = -rec.normal;
 			ni_over_nt = ref_idx;
+			cosine = ref_idx * dot(r_in.direction(), rec.normal)
+				/ r_in.direction().length();
 		}
-		else
-		{
+		else {
 			outward_normal = rec.normal;
-			ni_over_nt = 1.0f / ref_idx;
+			ni_over_nt = 1.0 / ref_idx;
+			cosine = -dot(r_in.direction(), rec.normal)
+				/ r_in.direction().length();
 		}
-		if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted))
-		{
+
+		if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+			reflect_prob = schlick(cosine, ref_idx);
+		}
+		else {
+			reflect_prob = 1.0;
+		}
+
+		if (random_double() < reflect_prob) {
+			scattered = ray(rec.p, reflected);
+		}
+		else {
 			scattered = ray(rec.p, refracted);
 		}
-		else
-		{
-			scattered = ray(rec.p, reflected);
-			return false;
-		}
+
 		return true;
 	}
 
--- ray_tracing_in_one_weekend.cpp
+++ ray_tracing_in_one_weekend.cpp
@@ -22,11 +22,50 @@
 
 #define EPS 0.001f
 #define OUTPUT_FILE_NAME "output.png"
-#define WIDTH 800
-#define HEIGHT 400
+#define WIDTH 1200
+#define HEIGHT 800
 #define NS 10
 
 using namespace std;
+
+hittable* random_scene() {
+	int n = 500;
+	hittable** list = new hittable * [n + 1];
+	list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = random_double();
+			vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) {  // diffuse
+					list[i++] = new sphere(center, 0.2,
+						new lambertian(vec3(random_double() * random_double(),
+							random_double() * random_double(),
+							random_double() * random_double())
+						)
+					);
+				}
+				else if (choose_mat < 0.95) { // metal
+					list[i++] = new sphere(center, 0.2,
+						new metal(vec3(0.5 * (1 + random_double()),
+							0.5 * (1 + random_double()),
+							0.5 * (1 + random_double())),
+							0.5 * random_double()));
+				}
+				else {  // glass
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+		}
+	}
+
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+	return new hittable_list(list, i);
+}
 
 vec3 color(const ray& r, hittable* world, int depth) {
 	hit_record rec;
@@ -55,13 +94,14 @@ void draw_scene()
 	vec3 origin(0.0, 0.0, 0.0);
 	unsigned char* imageBuffer = new unsigned char[WIDTH * HEIGHT * 4]();
 
-	hittable * list[4];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-	hittable * world = new hittable_list(list, 4);
-	camera cam;
+	vec3 lookfrom(13, 2, 3);
+	vec3 lookat(0, 0, 0);
+	float dist_to_focus = 10.0;
+	float aperture = 0.1;
+
+	camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(WIDTH) / float(HEIGHT), aperture, dist_to_focus);
+
+	hittable* world = random_scene();
 
 	for (int x = 0; x < WIDTH; x++)
 	{
@@ -92,10 +132,6 @@ void draw_scene()
 
 	stbi_write_png(OUTPUT_FILE_NAME, WIDTH, HEIGHT, 4, imageBuffer, WIDTH * 4);
 	delete[] imageBuffer;
-	delete list[0];
-	delete list[1];
-	delete list[2];
-	delete list[3];
 	delete world;
 }
 
