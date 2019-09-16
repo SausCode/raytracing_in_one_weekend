711b45c1080743f42af462836b756fd953621b76
John Isaac Cunningham <jicunnin@calpoly.edu>
2019-09-15 19:02:40 -0700

    add camera, anti-aliasing, and camera

--- ray_tracing_in_one_weekend.cpp
+++ ray_tracing_in_one_weekend.cpp
@@ -7,6 +7,8 @@
 #include "hittable.hpp"
 #include "hittable_list.hpp"
 #include "sphere.hpp"
+#include "camera.hpp"
+#include "random.hpp"
 
 #include <iostream>
 #include <fstream>
@@ -15,14 +17,26 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#define EPS 0.001f
+
 using namespace std;
+
+vec3 random_in_unit_sphere()
+{
+	vec3 p;
+	do {
+		p = 2.0f * vec3(random_float(), random_float(), random_float()) - vec3(1, 1, 1);
+	} while (p.square_length() >= 1.0);
+	return p;
+}
 
 vec3 color(const ray& r, hittable* world)
 {
 	hit_record rec;
-	if (world->hit(r, 0.0, std::numeric_limits<float>::max(), rec))
+	if (world->hit(r, EPS, std::numeric_limits<float>::max(), rec))
 	{
-		return 0.5f* vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5f* color(ray(rec.p, target - rec.p), world);
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5f * (unit_direction.y() + 1);
@@ -31,8 +45,9 @@ vec3 color(const ray& r, hittable* world)
 
 
 int main() {
-	int nx = 800;
-	int ny = 400;
+	int nx = 200;
+	int ny = 100;
+	int ns = 100;
 	vec3 lower_left_corner(-2.0, -1.0, -1.0);
 	vec3 horizontal(4.0, 0.0, 0.0);
 	vec3 vertical(0.0, 2.0, 0.0);
@@ -43,16 +58,23 @@ int main() {
 	list[0] = new sphere(vec3(0, 0, -1), 0.5f);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hittable* world = new hittable_list(list, 2);
+	camera cam;
 
 	for (int x = 0; x < nx; x++)
 	{
 		for (int y = 0; y < ny; y++)
 		{
-			float u = float(x) / float(nx);
-			float v = float(y) / float(ny);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 p = r.point_at_parameter(2.0);
-			vec3 col = color(r, world);
+			vec3 col(0,0,0);
+			for (int s = 0; s < ns; s++)
+			{
+				float u = float(x + random_float()) / float(nx);
+				float v = float(y + random_float()) / float(ny);
+				ray r = cam.get_ray(u, v);
+				col += color(r, world);
+			}
+			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+
 			unsigned char ir = unsigned char(255.99 * col[0]);
 			unsigned char ig = unsigned char(255.99 * col[1]);
 			unsigned char ib = unsigned char(255.99 * col[2]);
@@ -61,6 +83,7 @@ int main() {
 			imageBuffer[x * 4 + (ny - 1 - y) * 4 * nx + 2] = ib;
 			imageBuffer[x * 4 + (ny - 1 - y) * 4 * nx + 3] = 255;
 		}
+		std::cout << "x = " << x << "out of : " << nx << std::endl;
 	}
 
 	stbi_write_png("output.png", nx, ny, 4, imageBuffer, nx * 4);
